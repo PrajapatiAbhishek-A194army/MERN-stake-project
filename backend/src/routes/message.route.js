258b3b6eb8541a3fe9dea17ad027739d52f7179a
PrajapatiAbhishek-A194army <prajapatibhagudevi33@gmail.com>
2025-05-24 21:24:08 +0530

    Update message.route.js

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -2,6 +2,12 @@ import express from "express"
 import { protectRoute } from "../../middleware/auth.middleware.js";
 import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 const router=express.Router();
+router.param("id", (req, res, next, id) => {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Invalid ID format" });
+  }
+  next();
+});
 router.get("/users",protectRoute,getUsersForSidebar)
 router.get("/:id",protectRoute,getMessages)
 router.post("/send/:id",protectRoute,sendMessage)
